1b5eb716d63155045643af4135d213818af84aaa
Daniel Svensson <daniel.svensson@hotmail.se>
2021-03-15 13:50:48 +0100

    more days

---
+++ AdventOfCode/AdventOfCode2020/10/Program.fs
@@ -0,0 +1,56 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open System.IO
+
+[<EntryPoint>]
+let main argv =
+
+    let input = 
+        File.ReadAllLines "input.txt"
+        |> Array.map Int32.Parse
+        
+    Array.sortInPlace input
+    let adapters = Array.concat [
+            [|0|]; 
+            input; 
+            [|3 + (Array.last input)|]
+        ]
+
+    let rec getDifferences idx prev (counts:int array) = 
+        if (idx = adapters.Length) then counts
+        else 
+            let item = adapters.[idx]
+            let diff = item - prev
+            counts.[diff] <- counts.[diff] + 1 
+            getDifferences (idx + 1) item counts
+
+    let counts = 
+        Array.zeroCreate<int> 4
+        |> getDifferences 1 0
+
+    
+    printfn "counts %A multiplied %d" counts (counts.[1] * counts.[3]) 
+
+
+    // DP problem
+    // d(x) där x är en adapter
+    // -3 -2 -1 0
+    // d( <= 0 ) = 1
+    let d = Array.zeroCreate<UInt64> adapters.Length
+    d.[0] <- 1UL
+
+    let calcCombinations idx =
+        let current = adapters.[idx]
+        let rec getCombinations i sum = 
+            if i < 0 || adapters.[i] < current - 3 then sum
+            else getCombinations (i-1)  (sum + d.[i]) 
+
+        getCombinations (idx - 1) 0UL
+
+    for i in 1..adapters.Length - 1 do
+        d.[i] <- calcCombinations i
+
+    printfn "d = %A\n%d" d (Array.last d)
+
+    0 // return an integer exit code
---
+++ AdventOfCode/AdventOfCode2020/11/Program.fs
@@ -0,0 +1,89 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+[<Literal>]
+let EMPTY = 'L'
+[<Literal>]
+let OCCUPIED = '#'
+
+[<EntryPoint>]
+let main argv =
+
+    let input = IO.File.ReadAllLines "input.txt"
+
+    let seats = input |> Array.map (fun x -> x.ToCharArray())
+
+    let isOccupied (state: char[][]) x y =
+        y >= 0 && y < state.Length
+            && x >= 0 && x < state.[y].Length 
+            && state.[y].[x] = '#'
+
+    let neighbors = 
+        [|
+            for x in -1..1 do
+                for y in -1..1 do
+                    if (x <> 0 || y <> 0) then yield (x,y) 
+        |]
+
+    let countNeighbors state x y =
+        let mutable count = 0
+        for (dx,dy) in neighbors do
+            if isOccupied state (x + dx) (y + dy) then
+                count <- count + 1
+        count
+
+    let rec checkDirection  (state: char[][]) x y dx dy = 
+        y >= 0 && y < state.Length
+        && x >= 0 && x < state.[y].Length 
+        && match state.[y].[x] with
+        | OCCUPIED -> true
+        | EMPTY -> false
+        | _ -> checkDirection state (x + dx) (y + dy) dx dy
+            
+    let countNeighbors2 state x y =
+        let mutable count = 0
+        for (dx,dy) in neighbors do
+            if checkDirection state (x + dx) (y + dy) dx dy then
+                count <- count + 1
+        count
+
+    let flipAll state =
+        let newState = state |> Array.map Array.copy
+        let mutable change = false
+        for y in 0..state.Length - 1 do
+            for x in 0..state.[y].Length - 1 do
+                match state.[y].[x] with 
+                | 'L' when (countNeighbors2 state x y) = 0 ->
+                    newState.[y].[x] <- '#'
+                    change <- true
+                | '#' when (countNeighbors2 state x y) >= 5 ->
+                    newState.[y].[x] <- 'L'
+                    change <- true
+                | _ -> ()
+        (newState, change)
+
+    let print state step =
+        printfn "%d" step
+        state 
+        |> Array.iter (fun row -> 
+            row |> Array.iter (printf "%c")
+            printfn ""
+            )
+
+    let rec solve state flips =
+    //    print state flips
+        let (s2, changed) = flipAll state
+        if (changed) then solve s2 (flips + 1)
+        else (flips, state)
+        
+    let (num, final) = solve seats 1
+
+    printfn "After %d the final state is" num
+    print final num
+
+    final
+    |> Array.sumBy (fun row -> row |> Array.sumBy (fun x -> if x = OCCUPIED then 1 else 0))
+    |> printfn "found %d occupied"
+    
+    0 // return an integer exit code
