7056b2d1a8f872f6d8a4ba67d9ab975bf71a458b
Daniel Svensson <daniel.svensson@hotmail.se>
2021-09-09 20:00:19 +0200

    15 16

---
+++ AdventOfCode/AdventOfCode2020/15/Program.fs
@@ -0,0 +1,52 @@
+﻿// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
+
+open System
+
+let maxIterations = 30000000
+
+[<Struct>]
+type State = { LastUsed : int[] ; Next :int }
+
+let init numbers = {
+    LastUsed = 
+        numbers
+        |> Seq.take (Array.length numbers - 1)
+        |> Seq.indexed
+        |> Seq.fold (fun state (idx, num) ->
+            state.[num] <- (idx+1) 
+            state
+        ) (Array.zeroCreate<int> maxIterations);
+    Next = Array.last numbers
+    }
+
+let next prev turn (state:int[]) =
+    match state.[prev] with
+    | 0 -> 0
+    | lastUsed -> (turn - lastUsed)
+
+let step turn state =
+    let n = next state.Next turn state.LastUsed 
+    state.LastUsed.[state.Next] <- turn 
+    {LastUsed = state.LastUsed; Next = n}    
+    
+
+[<EntryPoint>]
+let main argv =
+
+    IO.File.ReadAllLines "sample.txt"
+    |> Seq.iter (fun row -> 
+        let input = 
+            row.Split(',')
+            |> Array.map Int32.Parse
+
+        let mutable state = init input
+        for i in input.Length..2019 do
+            state <- step i state
+        
+        let num2020 = state.Next
+        for i in 2020..(30000000 - 1) do
+            state <- step i state
+
+        printfn "Input %A gives %d and %d" input num2020 state.Next
+    )
+    0 // return an integer exit code
---
+++ AdventOfCode/AdventOfCode2020/16/Program.fs
@@ -0,0 +1,79 @@
+﻿// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
+
+open System
+
+// Define a function to construct a message to print
+[<Struct>]
+type Range = {Min:int; Max:int}
+
+[<Struct>]
+type Rule = {Name: string; Range1: Range; Range2: Range}
+
+type Ticket = int[]
+
+module Rule =
+    let parse (str:string) =
+        let parseRange (s:string) =
+            let parts = s.Split('-')
+            {Min = parts.[0] |> int ; Max = parts.[1] |> int}
+
+        let nameAndRanges = str.Split(": ")
+        let ranges = nameAndRanges.[1].Split(" or ")
+        {Name = nameAndRanges.[0]; Range1 = (parseRange ranges.[0]); Range2 = (parseRange ranges.[1])}
+
+    let inRange i range =
+        range.Min <= i && i <= range.Max
+
+    let valid i rule =
+        (inRange i rule.Range1) || (inRange i rule.Range2)
+
+module Ticket =
+    let parse (str:string) =
+        str.Split(',')
+        |> Array.map int
+
+type ProblemInput = {Rules: Rule[]; YourTicket: Ticket; NearbyTickets: Ticket[]}
+
+let parse (lines:string[]) =
+    let mutable i = 0
+
+    let rules = 
+        lines 
+        |> Array.takeWhile (not << String.IsNullOrEmpty)
+        |> Array.map Rule.parse
+
+    let tickets = 
+        lines
+        |> Array.skip (rules.Length + 5)
+        |> Array.map Ticket.parse
+    {
+    Rules = rules
+    YourTicket = Ticket.parse lines.[rules.Length + 2]
+    NearbyTickets = tickets
+    }
+
+
+let completelyInvalid rules ticket = 
+    ticket
+    |> Array.forall (fun i -> rules |> Array.forall (fun r -> not (Rule.valid i r)))
+
+[<EntryPoint>]
+let main argv =
+
+    let input = IO.File.ReadAllLines "input.txt"
+
+    let problem = parse input
+
+    let ticketSum rules ticket = 
+        ticket |> 
+        Array.sumBy (fun i -> 
+            if (Array.forall (not << (Rule.valid i)) rules) then i
+            else 0
+        )
+
+    problem.NearbyTickets
+    |> Array.sumBy (ticketSum problem.Rules)
+    |> printfn "Sum is %d"
+
+
+    0 // return an integer exit code
