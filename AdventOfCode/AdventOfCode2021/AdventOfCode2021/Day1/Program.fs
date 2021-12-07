bd45ef28b99e922c6973341a9a1ce32641aa9dc3
Daniel Svensson <daniel.svensson@hotmail.se>
2021-12-07 22:04:42 +0100

    more fun

---
+++ AdventOfCode/AdventOfCode2021/AdventOfCode2021/Day1/Program.fs
@@ -0,0 +1,30 @@
+﻿// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
+
+open System
+open System.IO
+
+
+[<EntryPoint>]
+let main argv =
+
+    let input = 
+        File.ReadAllLines "In.txt" 
+        |> Array.map Int32.Parse
+
+    //let numIncreases =
+    //    Array.zip input (Array.skip 1 input)
+    //    |> Array.sumBy (fun (a,b) -> if a < b then 1 else 0)
+    let countIncreases (arr:int[]) = 
+        let mutable c  = 0
+        for i in 1..arr.Length-1 do
+            c <- if (arr.[i] > arr.[i - 1]) then (c + 1) else c
+        c
+
+    printfn "Increases %d" (countIncreases input)
+
+    let window3sum =
+        input |> Array.windowed 3 |> Array.map Array.sum
+
+    printfn "Increases %d" (countIncreases window3sum)
+
+    0 // return an integer exit code
---
+++ AdventOfCode/AdventOfCode2021/AdventOfCode2021/Day2/Program.fs
@@ -0,0 +1,57 @@
+﻿// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
+
+open System
+open System.IO
+
+type Direction = Forward | Up | Down
+type Action = {Direction: Direction; Units : uint}
+type State = {Distance: uint; Depth : uint }
+
+let parseAction (str:string) =
+    let parts = str.Split(' ')
+    let direction = 
+        match parts.[0] with
+        | "up" -> Up
+        | "down" -> Down
+        | "forward" -> Forward
+        | _ -> failwith "invalid direction"
+    {Direction = direction; Units = (uint parts.[1])}
+
+let move state action =
+    match action.Direction with
+    | Up when state.Depth >= action.Units -> {state with Depth = state.Depth + action.Units} 
+    | Down -> {state with Depth = state.Depth + action.Units} 
+    | Forward -> {state with Distance = state.Distance + action.Units} 
+    | _ -> failwith "Invalid move"
+
+// part 2
+type State2 = {Distance: uint; Depth : uint; Aim : uint}
+let move2 state action =
+    match action.Direction with
+    | Up -> {state with Aim=  state.Aim - action.Units} 
+    | Down -> {state with Aim = state.Aim + action.Units} 
+    | Forward -> {state with Distance = state.Distance + action.Units; Depth = state.Depth + state.Aim * action.Units} 
+
+[<EntryPoint>]
+let main argv =
+
+    let solve1 file = 
+        File.ReadAllLines file 
+        |> Array.map parseAction
+        |> Array.fold move {Distance = 0u; Depth = 0u}
+        |> (fun state -> printfn "%s position is %A, result %d" file state (state.Distance * state.Depth))
+
+    solve1 "Sample.txt"    
+    solve1 "In.txt"
+
+    let solve2 file = 
+        File.ReadAllLines file 
+        |> Array.map parseAction
+        |> Array.fold move2 {Distance = 0u; Depth = 0u; Aim = 0u}
+        |> (fun state -> printfn "%s position is %A, result %d" file state (state.Distance * state.Depth))
+
+        
+    solve2 "Sample.txt"    
+    solve2 "In.txt"
+
+    0 // return an integer exit code
